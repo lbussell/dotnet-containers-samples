c89c07a81ea09b50d8a7f13e361aec96f27dd592
Logan Bussell <loganbussell@microsoft.com>
2025-10-27 19:24:48 -0700

    Add pretty printing for image size

--- src/Generator/ImageSize.cs
+++ src/Generator/ImageSize.cs
@@ -3,4 +3,20 @@
 
 namespace Generator;
 
-public readonly record struct ImageSize(long Bytes);
+public readonly record struct ImageSize(long Bytes)
+{
+    public override string ToString()
+    {
+        const long KB = 1024;
+        const long MB = KB * 1024;
+        const long GB = MB * 1024;
+
+        return Bytes switch
+        {
+            >= GB => $"{Bytes / (double)GB:F2} GB",
+            >= MB => $"{Bytes / (double)MB:F2} MB",
+            >= KB => $"{Bytes / (double)KB:F2} KB",
+            _ => $"{Bytes} B"
+        };
+    }
+}
